febe0c0851d7731bc579cd41bf43a423fc61184c
boriskisiov <boriskisiov@gmail.com>
2023-10-24 15:10:13 +0300

    Add project files.

---
+++ Travel savings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Travel_savings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double neededSum = double.Parse(Console.ReadLine());
+                double collectedSum = 0;
+                while (collectedSum < neededSum) {
+                    collectedSum += double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Collected:{collectedSum :f2}");
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
